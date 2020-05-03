a5c1231f6c96cf0b2a00dcee6e8162134412779a
Laurent Evrard <evrardlaurent77@gmail.com>
2020-05-03 09:47:36 +0200

    feat(app): add healthcheck and build

--- infrastructure/services/logger.py
+++ infrastructure/services/logger.py
@@ -1,16 +1,11 @@
-import os
 import sys
-import logging as std_logging
 
 from loguru import logger as logging
-from sentry_sdk import capture_exception, init
-from sentry_sdk.integrations.logging import EventHandler
 
 from settings import dev
 
 
 _logger = None
-init(os.getenv("SentryDN"), default_integrations=(not dev()))
 
 
 def logger():  # noqa: D103
@@ -26,29 +21,4 @@ def logger():  # noqa: D103
             level="DEBUG",
         )
         _logger.add(sys.stdout, level="DEBUG" if dev() else "INFO")
-        if not dev():
-            handler = EventHandler()
-            _logger.add(handler, level="ERROR", format="{name} - {message}")
     return _logger
-
-
-def report_exception(exception: Exception) -> None:
-    """Report an exception to sentry.
-
-    Args:
-        exception: the exception to be reported
-
-    Effects:
-        The exception is reported in sentry
-
-    """
-    assert isinstance(exception, Exception)
-
-    if not dev():
-        capture_exception(exception)
-
-
-class InterceptHandler(std_logging.Handler):
-    def emit(self, record):
-        logger_opt = logger().opt(depth=6, exception=record.exc_info)
-        logger_opt.log(record.levelno, record.getMessage())
--- settings.py
+++ settings.py
@@ -4,13 +4,6 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-PASSWORD_RECOVERY_EMAIL_TEMPLATE = os.getenv(
-    "PASSWORD_RECOVERY_EMAIL_TEMPLATE", "password_recovery"
-)
-PASSWORD_RECOVERY_FRONT_URL = os.getenv(
-    "PASSWORD_RECOVERY_FRONT_URL", "https://graphboard.owl-int.com/recovery"
-)
-
 
 def dev():
     return os.getenv("dev", "False") == "True"
@@ -30,15 +23,3 @@ def mongo_username():
 
 def mongo_password():
     return os.getenv("mongo_password", None)
-
-
-def redis_url():
-    return os.getenv("redis", "localhost")
-
-
-def redis_password():
-    return os.getenv("redis_password", None)
-
-
-def redis_database():
-    return int(os.getenv("redis_database", "0"))
